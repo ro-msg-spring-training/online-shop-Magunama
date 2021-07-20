bc72ff6d9b0f4a67bf4abf4bceb5e7a1e1b32a6e
Robert-Cristian Ciama <robert.ciama00@e-uvt.ro>
2021-07-20 10:40:30 +0300

    Enabling H2 console & file storage

---
+++ src/main/java/ro/msg/learning/shop/WebSecurityConfig.java
@@ -0,0 +1,32 @@
+package ro.msg.learning.shop;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/h2-console/**")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin();
+
+        // allow access to h2 console
+        http.csrf()
+                .ignoringAntMatchers("/h2-console/**");
+
+        http.headers()
+                .frameOptions()
+                .sameOrigin();
+    }
+}
